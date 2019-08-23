b67755ea99922d4dbc3446d6317601f2db68645f
ren <1952933329@qq.com>
2019-08-23 21:50:03 +0800

    文件整合

--- 第二周_信息提取/03pycharm综合实验/BeautifulSoup_first.py
+++ 第二周_信息提取/03pycharm综合实验/BeautifulSoup_first.py
@@ -5,6 +5,7 @@ r = requests.get('http://python123.io/ws/demo.html')
 demo = r.text
 
 soup = BeautifulSoup(demo, 'html.parser')
-for son in soup.body.descendants:
-    print(son)
+# for son in soup.body.descendants:
+#     print(son)
 
+i = 0
