596752d85d74d621ce1f7e4402e52d0fa030ca7c
ren <1952933329@qq.com>
2019-08-28 22:02:05 +0800

    新增股票

--- 03第三周_正则表达式/03综合实验_淘宝爬取/taobao.py
+++ 03第三周_正则表达式/03综合实验_淘宝爬取/taobao.py
@@ -43,7 +43,7 @@ def print_info(infolist):
 def main():
     info_list = []
     depth = 1
-    good = '羽绒服 男 降序'
+    good = '男鞋'
     kv = {'cookie': 'thw=cn; cna=PdvfFbP8qmcCAd8DTaG+cEn8; v=0; t=eb60b025571ebb9dc3322b10c80858ac; cookie2=1891b6fa7ced0e6ebcaa05f5b11b77d0; _tb_token_=57b16e34eeeb; unb=2002399149; uc3=nk2=q5ccXpBE&lg2=Vq8l%2BKCLz3%2F65A%3D%3D&vt3=F8dBy3MNgeVDnvIThW0%3D&id2=UUjRIG%2B7mmYCGw%3D%3D; csg=2ed2c55f; lgc=%5Cu4EFB%5Cu70B3%5Cu5148; cookie17=UUjRIG%2B7mmYCGw%3D%3D; dnk=%5Cu4EFB%5Cu70B3%5Cu5148; skt=1ccc3f4644d349c2; existShop=MTU2NjcwNTA3OA%3D%3D; uc4=id4=0%40U2ozl6EX%2BOswIFh%2FQnvHsJgSJ3sS&nk4=0%40qSIoWVsEltd4PVLrO3RXLYs%3D; tracknick=%5Cu4EFB%5Cu70B3%5Cu5148; _cc_=WqG3DMC9EA%3D%3D; tg=0; _l_g_=Ug%3D%3D; sg=%E5%85%8899; _nk_=%5Cu4EFB%5Cu70B3%5Cu5148; cookie1=UINO14AbOLBtlI6nnlE3EPmakbbnbtKHqHJareoNkiY%3D; enc=n%2FOKevWY8CEAmAyO%2BiOXy6de2a3MM0rfRXVsOExbrXB%2BdEI7TxggfGdMRT2jifUdKaRaNvKZG0iudAol5dXtKQ%3D%3D; hng=CN%7Czh-CN%7CCNY%7C156; mt=ci=74_1; swfstore=217968; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; JSESSIONID=99B39E924A302464DB0EE38F49597828; uc1=cookie14=UoTaHoyydCFjyQ%3D%3D&lng=zh_CN&cookie16=URm48syIJ1yk0MX2J7mAAEhTuw%3D%3D&existShop=false&cookie21=W5iHLLyFeYZ1WM9hVLhR&tag=8&cookie15=V32FPkk%2Fw0dUvg%3D%3D&pas=0; isg=BHFxKh1OiGL22yR2xrpdwRdegP3UXahxOIDYS1OGATjsepPMm658ocXYmE65sn0I; l=cBO1f8RVqJ2tfN0SBOCwdkf1SJ_TlIRVguP3Y6AWi_5QT1Av2i_OkorMeev6cjWFTsYB4CWT0IpTze3L8yDXnSFpne1VivC..; whl=-1%260%260%261566706689827',
           'user-agent': "Mozilla/5.0"}
     start_url = 'https://s.taobao.com/search?q=' + good
---
+++ 03第三周_正则表达式/03综合实验_淘宝爬取/股票.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import traceback
+
+
+def get_html_text(url, code='utf-8'):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = code
+        return r.text
+    except:
+        return ""
+
+
+def get_stock_list(stoList, stock_list_url):
+    """
+    获取股票代码列表
+    :param stoList: 存储代码的列表
+    """
+    html = get_html_text(stock_list_url,code='GB2312')
+    soup = BeautifulSoup(html, 'html.parser')
+    a_set = soup.find_all('a')  # 返回由a标签构成的resultset
+    for link in a_set:
+        try:
+            href = link.attrs['href']
+            num = re.findall(r'[s][hz]\d{6}', href)[0]
+            stoList.append(num)
+        except:pass
+
+
+def get_stock_info(stoList, stock_info_url, file_path, num_limit= 10):
+    """
+    将 股票名称 与 股票代码 封装成 键值对 存储到 字典-->文件中
+    :param stoList:  代码列表
+    :param stock_info_url: 个股根路径
+    :param file_path: 保存文件的路径
+    :return:
+    """
+    infoDict = {}
+    count = 0
+    boot_url = stock_info_url
+
+    for stock_num in stoList:
+        if count < num_limit:
+            url = boot_url + stock_num + '.html'
+            html = get_html_text(url)
+            soup = BeautifulSoup(html, 'html.parser')
+            try:
+                sto_info = soup.find_all('div', attrs={'class': 'stock-bets'})[0]
+                name = sto_info.find_all('a', attrs={'class': 'bets-name'})[0]
+                infoDict['股票名称'] = name.text.split()[0]
+                keys = sto_info.find_all('dt')
+                values = sto_info.find_all('dd')
+                for i in range(len(keys)):
+                    infoDict[keys[i].text] = values[i].text.split()[0]
+                with open(file_path, 'a') as f:
+                    f.write(str(infoDict) + '\n')
+                    count = count + 1
+                    print("\r当前进度: {:.2f}%".format(count * 100 / num_limit), end="")
+            except:
+                count = count + 1
+                # traceback.print_exc()
+                print("\r当前进度: {:.2f}%".format(count * 100 / num_limit), end="")
+                continue
+    return ""
+
+
+def main():
+    stock_list_url = 'http://quote.eastmoney.com/stock_list.html'
+    stock_info_url = 'https://gupiao.baidu.com/stock/'
+    file_path = 'stock_info.txt'
+    stoList = []
+    get_stock_list(stoList, stock_list_url)
+    get_stock_info(stoList, stock_info_url, file_path,num_limit=50)
+
+main()
